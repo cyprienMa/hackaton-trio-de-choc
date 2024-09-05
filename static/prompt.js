6579e86c046bd3208b446ff9b88b7abfb8a64e40
Clément Mazzocchi <mazzocchi.clement@gmail.com>
2024-09-05 15:09:58 +0200

    prets pour la fusioooooon

--- static/prompt.js
+++ static/prompt.js
@@ -130,3 +130,18 @@ function clearContent() {
 
 questionButton.addEventListener("click", handleQuestionClick);
 qcmButton.addEventListener("click", handleQCMClick);
+
+
+window.onscroll = function () {
+  const button = document.getElementById('backToTop');
+  if (document.documentElement.scrollTop > 200) {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
+};
+
+// Fonction pour faire défiler vers le haut
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
